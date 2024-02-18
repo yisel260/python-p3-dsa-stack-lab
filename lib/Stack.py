6a0b0f3c632fde215fafde3b7092f72df07bc6dc
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-02-17 20:30:53 -0500

    completed lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,47 @@
 class Stack:
 
+
     def __init__(self, items = [], limit = 100):
-        pass
+       self.dictionary ={}
+       for item in items:
+           self.dictionary[item] = True
+       self.items = items 
+       self.limit = limit 
 
     def isEmpty(self):
-        pass
+        return len(self.dictionary) == 0
+          
 
     def push(self, item):
-        pass
+        if len(self.dictionary) < self.limit:
+            self.dictionary[item] = True  
+            self.items.append(item) 
+            return self
+        else:
+            print("Stack is already full")
+            return False
 
     def pop(self):
-        pass
+        if self.items:
+            item = self.items.pop()
+            self.dictionary.pop(item, None)
+            return item
+        else:
+           return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+       return len(self.dictionary)
 
     def full(self):
-        pass
+        return len(self.dictionary) == self.limit 
+
 
     def search(self, target):
-        pass
+       if target in self.dictionary:
+           position = len(self.items) - self.items.index(target) - 1
+           return position
+       else:
+        return -1
